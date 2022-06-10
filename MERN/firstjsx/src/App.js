3808551c88e6be649acb99d599d024452451fcd5
swright96 <swright96@live.com>
2022-06-10 04:21:15 -0600

    initial commit

---
+++ MERN/firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+	return (
+		<div className="container">
+			<h1>Hello Dojo!</h1>
+			<h2>Things I need to do:</h2>
+			<ul>
+				<li>Learn React</li>
+				<li>Eat some twinkies</li>
+				<li>Take a small jog</li>
+				<li id="danger">Feed my wife</li>
+			</ul>
+		</div>
+	)
+}
+
+export default App
